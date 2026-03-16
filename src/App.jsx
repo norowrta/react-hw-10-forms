a6c9536acec07d7b62d64cae4a4a2fbc72f2fc8f
norowrta <m.bohatovv@gmail.com>
2026-03-16 10:59:31 +0100

    feat: add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,19 @@ export default class App extends Component {
     filter: "",
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
 

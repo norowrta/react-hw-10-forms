7976f048e9eac7354eadaf07e3075ff6799a57a4
norowrta <m.bohatovv@gmail.com>
2026-03-28 18:33:36 +0100

    fix: localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import css from "./app.module.css";
 
 import { Component } from "react";
 
+const defaultContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+const getInitialContacts = () => {
+  const savedContacts = window.localStorage.getItem("contacts");
+  return savedContacts !== null ? JSON.parse(savedContacts) : defaultContacts;
+};
+
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: getInitialContacts(),
     filter: "",
   };
-
-  componentDidMount() {
-    const savedContacts = localStorage.getItem("contacts");
-    if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.contacts !== this.state.contacts) {
